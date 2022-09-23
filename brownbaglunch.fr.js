5b514e15a015ce8f48e8eb73f7d0430099e9ce3c
Marc BOUVIER <m.bouvier.dev@gmail.com>
2022-09-24 00:36:06 +0200

    Update brownbaglunch.fr.js

--- brownbaglunch.fr.js
+++ brownbaglunch.fr.js
@@ -2,7 +2,7 @@
 session = {
             title: 'Docker sur le poste du développeur',
             abstract: 'Vous avez entendu parler de Docker, vous avez été formé dessus mais vous ne savez pas à quoi ça pourrait servir dans votre quotidien de développeur?\n\nDans cette présentation vous verrez\n\n- Des cas d’utilisation concrets (ex. test containers, frontend + keycloak + spring boot, test de jobs CI, ...)\n- Des exemples / démos de certains de ces usages\n\nLa présentation sera suivie d’une session de questions-réponses.',
-            tags: ['Docker', 'Docker-Compose','Spring Boot','Java','VueJs','Typescript', 'Keycloak', 'OpenId Connect', 'OAuth 2', 'Asciidoctor],
+            tags: ['Docker', 'Docker-Compose','Spring Boot','Java','VueJs','Typescript', 'Keycloak', 'OpenId Connect', 'OAuth 2', 'Asciidoctor'],
             lang: ['fr'],
            }
 
