8e70e2abac20b5d75e8cd5f38fbf5647103375d7
Marc BOUVIER <m.bouvier.dev@gmail.com>
2022-09-24 00:30:36 +0200

    Create brownbaglunch.fr.js

---
+++ brownbaglunch.fr.js
@@ -0,0 +1,7 @@
+// Ajouter à : https://github.com/brownbaglunch/bblfr_data
+session = {
+            title: 'Docker sur le poste du développeur',
+            abstract: 'TODO',
+            tags: ['Docker', 'Docker-Compose','Spring Boot','Java','VueJs','Typescript', 'Keycloak', 'OpenId Connect', 'OAuth 2'],
+            lang: ['fr'],
+           }
